f428a5536998b9c6c5dcb5364d2772fec4604e66
Generoso Martello <generoso.martello@gmail.com>
2018-09-22 21:05:48 +0200

    updated appveyor

---
+++ SerialPortLib/nuget_pack.ps1
@@ -0,0 +1,13 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\SerialPortLib\bin\Release\SerialPortLib.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\SerialPortLib\SerialPortLib.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\SerialPortLib\SerialPortLib.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\SerialPortLib\SerialPortLib.compiled.nuspec
+
