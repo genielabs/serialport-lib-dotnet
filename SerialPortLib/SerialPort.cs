98c77b4754ab5ddf3510a29df1aecff86dc7b4d8
Generoso Martello <generoso.martello@gmail.com>
2019-02-12 20:19:30 +0100

    Added XML comment for DataBits enum

--- SerialPortLib/SerialPort.cs
+++ SerialPortLib/SerialPort.cs
@@ -36,9 +36,21 @@ namespace SerialPortLib
     /// </summary>
     public enum DataBits
     {
+        /// <summary>
+        /// DataBits 5.
+        /// </summary>
         Five = 5,
+        /// <summary>
+        /// DataBits 6.
+        /// </summary>
         Six,
+        /// <summary>
+        /// DataBits 7.
+        /// </summary>
         Seven,
+        /// <summary>
+        /// DataBits 8.
+        /// </summary>
         Eight,
     }
 
